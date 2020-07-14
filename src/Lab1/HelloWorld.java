9eb4470d7f60b7c3c3a51aabdf2f0fabebf7ca80
soraya <soraya.pr@rmutsvmail.com>
2020-07-14 17:53:12 +0700

    add code 2 lines

--- src/Lab1/HelloWorld.java
+++ src/Lab1/HelloWorld.java
@@ -5,6 +5,12 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World !!!");
         System.out.println("Soraya");
+        System.out.println("คณะเทคโนโลยีสารสนเทศ");
+        System.out.println("มหาวิทยาลัยเทคโนโลยีราชมงคลศรีวิชัย");
+
+
+
+
 
     }
 }

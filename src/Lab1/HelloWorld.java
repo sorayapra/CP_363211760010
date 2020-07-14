f07fb1573c1d367e9609ff70c1178752768379b6
soraya <soraya.pr@rmutsvmail.com>
2020-07-14 17:39:39 +0700

    Initial commit

---
+++ src/Lab1/HelloWorld.java
@@ -0,0 +1,11 @@
+package Lab1;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World !!!");
+        System.out.println("Soraya");
+
+    }
+}
+

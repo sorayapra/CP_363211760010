3fe69e08033dda194efd99a558e0939a607bf328
LabCom_MT-4 <soraya.pr@rmvtsvmail.com>
2020-08-11 18:43:49 +0700

    Lab_11

---
+++ src/Lab3/Example_if.java
@@ -0,0 +1,21 @@
+package Lab3;
+
+public class Example_if {
+  public static void main(String[] args) {
+
+      int x, y;
+
+      x = 10;
+      y = 20;
+
+      if (x > y)
+          //.......statement of if
+          System.out.println("x grater than y.");
+
+         System.out.println("Good Bay.");
+
+
+
+  }//main
+}//class
+
